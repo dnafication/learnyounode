567deff6c603a1f69f4fad2f2eb06d710d8b497f
dnafication <>
2019-03-14 11:34:24 +1100

    finished hurrah!!

--- 02babySteps.js
+++ 02babySteps.js
@@ -1,5 +1,12 @@
 // console.log(process.argv);
 
+/**
+ * ## BABY STEPS (Exercise 2 of 13)
+
+  Write a program that accepts one or more numbers as command-line arguments
+  and prints the sum of those numbers to the console (stdout).
+ */
+
 let total = 0;
 process.argv.forEach((element, idx) => {
   if (idx > 1) {
